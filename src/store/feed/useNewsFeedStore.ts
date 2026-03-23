429f36242f06555424a41f2c4a98d65d7890a1aa
ramil villahermosa <villahermosaramil2019@gmail.com>
2026-03-23 20:25:49 +0800

    addPost and display  UI on marketplace

--- src/store/feed/useNewsFeedStore.ts
+++ src/store/feed/useNewsFeedStore.ts
@@ -1,13 +1,17 @@
 import { create } from "zustand";
-import type { FeedStore } from "./actions/utility/types";
+import type { AddPostData, FeedStore, } from "./actions/utility/types";
 import { fetchFeed } from "./actions/fetchFeeds";
+import { addPost,  } from "./actions/addPost";
 
 export const useNewsFeedStore = create<FeedStore>((set) => ({
-  feeds: null,
+  feeds: [],
   loading: false,
   error: null,
 
   fetchFeed: async () => {
     await fetchFeed(set);
   },
+  addPost:async( postData: AddPostData)=>{
+    await addPost(set,postData)
+  }
 }));
