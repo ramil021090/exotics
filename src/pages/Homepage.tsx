ce074d6a9c3e82ab305596a760a6c41bff01c8d4
ramil villahermosa <villahermosaramil2019@gmail.com>
2026-04-15 16:03:03 +0800

    added edit button and remove excess components

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,11 @@
-import MarketPlace from "../features/marketplcae/MarketPlace";
+import Feeds from "../feeds/Feeds";
 import Subheader from "../ui/Subheader";
 
 const Homepage = () => {
   return (
-    <div className=" bg-slate-50 dark:bg-slate-900">
+    <div className=" bg-slate-50 dark:bg-slate-900 h-screen">
       <Subheader title="Homepage" />
-      <MarketPlace />
+      <Feeds />
     </div>
   );
 };
